f5564d4603f717c56e5525eaf0c726db399fb2e9
Schlabauke <annabelle.jeffre@gmail.com>
2021-11-16 16:05:26 +0100

    Lev1JavaScriptTemplates

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/* Lev1_1 JavaScript Templates */
+
+let vorname = "Annabelle"
+let surname = "J."
+let age = 32
+let height = 1.80
+let hobby = ["Cello spielen", "Wandern", "Puzzlen", "Yoga", "Rad fahren"]
+let meal = "Nudeln mit Pesto"
+let game = "Horizon Zero Dawn"
+let vacation = "Alpen"
+
+console.log(hobby)
+console.log("Mein Name ist " + vorname + " " + surname + "Ich bin " + age + " Jahre alt und " + height + "m groß. Am liebsten mache ich das: " + hobby + ".")
+console.log(`Mein liebstes PS4- Game ist: ${game}. Dazu esse ich gerne: ${meal}. Zum Wandern fahre ich am liebsten in die ${vacation}`)
+/* Lev1_2 JavaScript Templates */
+let lunatic = "lunatic is";
+let darkSide = "I'll see you on the dark side of the moon";
+let hall = "hall";
+let head = "in my head";
+let door = "You lock the door";
+let paperboy = "paperboy";
+let and = "And";
+let the = "The";
+
+
+
+let songtext = `Brain Damage Songtext
+${the} ${lunatic} on the grass
+${the} ${lunatic} on the grass
+Remembering games and daisy chains and laughs
+Got to keep the loonies on the path
+
+${the} ${lunatic} in the ${hall}
+${the} lunatics are in my ${hall}
+${the} paper holds their folded faces to the floor
+${and} every day the ${paperboy} brings more
+
+
+${and} if the dam breaks open many years too soon
+${and} if there is no room upon the hill
+${and} if your head explodes with dark forebodings too
+${darkSide}
+
+${the} ${lunatic} in my ${head}
+${the} ${lunatic} in my ${head}
+You raise the blade, you make the change
+You rearrange me 'til I'm sane
+${door}
+${and} throw away the key
+There's someone in my head but it's not me
+
+${and} if the cloud bursts, thunder in your ear
+You shout and no one seems to hear
+${and} if the band you're in starts playing different tunes
+${darkSide}
+`
+console.log(songtext)
